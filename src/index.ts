443bc3b730599acfba7e33b84555685e0a802632
Blake Knight <oss.ideas@gmail.com>
2024-05-24 21:35:12 -0500

    try removing parsing using config

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import { createSchema, createYoga } from "graphql-yoga";
 import { resolvers, typeDefs } from "./schema";
 
+export const config = {
+  api: {
+    // Disable body parsing (required for file uploads)
+    bodyParser: false,
+  },
+};
+
 const yoga = createYoga({
   graphqlEndpoint: "/api",
   schema: createSchema({ typeDefs, resolvers }),
 });
 
 const server = Bun.serve({
-  fetch: async (req) => {
-    // Workaround Vercel parsing the request body as JSON for us
-    return yoga.handleRequest(req, {});
-  },
+  fetch: yoga,
 });
 
 const endpointUrl = new URL(
