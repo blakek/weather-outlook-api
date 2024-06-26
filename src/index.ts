baa862021f6adf2379759b0753f656688488d384
Blake Knight <oss.ideas@gmail.com>
2024-06-25 23:54:14 -0500

    :test_tube: adding nadocast HTTPS image proxy

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,25 @@ import { schema } from "./schema";
 
 const yoga = createYoga({
   graphqlEndpoint: "/api/graphql",
+  landingPage: false,
   schema,
 });
 
 const server = Bun.serve({
-  fetch: yoga,
+  fetch: (req: Request) => {
+    const url = new URL(req.url);
+
+    if (url.pathname.startsWith("/api/nadocast-proxy")) {
+      // Proxies the HTTP-only website images to HTTPS
+      const upstreamUrl = new URL(
+        url.pathname.replace("/api/nadocast-proxy", "http://data.nadocast.com"),
+      );
+
+      return fetch(upstreamUrl.href);
+    }
+
+    return yoga(req);
+  },
 });
 
 const endpointUrl = new URL(
--- src/vercel-handler.ts
+++ src/vercel-handler.ts
@@ -4,6 +4,17 @@ import { schema } from "./schema";
 const yoga = createYoga({ schema });
 
 export function GET(request: Request) {
+  const url = new URL(request.url);
+
+  if (url.pathname.startsWith("/api/nadocast-proxy")) {
+    // Proxies the HTTP-only website images to HTTPS
+    const upstreamUrl = new URL(
+      url.pathname.replace("/api/nadocast-proxy", "http://data.nadocast.com"),
+    );
+
+    return fetch(upstreamUrl.href);
+  }
+
   return yoga.handleRequest(request, {});
 }
 
