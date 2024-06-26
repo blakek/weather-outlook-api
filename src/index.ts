2aa958118669bedd13433c0ecda2cacf1cb6b8fe
Blake Knight <oss.ideas@gmail.com>
2024-06-25 23:57:04 -0500

    :fire: revert changes

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,11 @@ import { schema } from "./schema";
 
 const yoga = createYoga({
   graphqlEndpoint: "/api/graphql",
-  landingPage: false,
   schema,
 });
 
 const server = Bun.serve({
-  fetch: (req: Request) => {
-    const url = new URL(req.url);
-
-    if (url.pathname.startsWith("/api/nadocast-proxy")) {
-      // Proxies the HTTP-only website images to HTTPS
-      const upstreamUrl = new URL(
-        url.pathname.replace("/api/nadocast-proxy", "http://data.nadocast.com"),
-      );
-
-      return fetch(upstreamUrl.href);
-    }
-
-    return yoga(req);
-  },
+  fetch: yoga,
 });
 
 const endpointUrl = new URL(
