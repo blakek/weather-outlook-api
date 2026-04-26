dde31e69d803565a04ace68f1adc60166ba6b7f2
Blake Knight <oss.ideas@gmail.com>
2026-04-26 12:23:54 -0500

    update test files to point at new format

--- test-files/download-files.bash
+++ test-files/download-files.bash
@@ -2,5 +2,4 @@
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-curl --output-dir "$__dir" -LO 'https://www.spc.noaa.gov/products/outlook/archive/2021/day1otlk_20210325_1630_cat.lyr.geojson'
-curl --output-dir "$__dir" -LO 'https://www.spc.noaa.gov/products/outlook/archive/2021/day1otlk_20210325_1630_{,sig}{wind,hail,torn}.lyr.geojson'
+curl --output-dir "$__dir" -LO 'https://www.spc.noaa.gov/products/outlook/archive/2026/day1otlk_20260315_1630_{cat,wind,hail,torn}.lyr.geojson'
